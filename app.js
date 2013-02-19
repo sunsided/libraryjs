77d4e3fd2db50a2d8a388953aa62f6189bb2c813
Markus Mayer <development@widemeadows.de>
2013-02-19 02:21:34 +0100

    reorganized the initial app configuration, added Stylus configuration and enabled pretty formatting during development

--- app.js
+++ app.js
@@ -17,35 +17,51 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path')
+    , stylus = require('stylus');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+// configure the app
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+// configure the middlewares
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session({ secret: "shhhhhhhhh!"}));
+app.use(express.csrf());
+app.use(app.router);
+app.use(stylus.middleware({
+    src: path.join(__dirname, 'public'),
+    compile: function (str, path) {
+        var verbose = app.get("env") == "development";
+        return stylus(str)
+            .set('filename', path)
+            .set('compress', true)
+            .set('warn', verbose)
+            .set('linenos', verbose);
+    }}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// configure the middlewares for development
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.locals({pretty: true});
+    app.use(express.errorHandler());
 });
 
+// configure the routes
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+// start the server
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
